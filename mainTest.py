d9d7dc9face4aecc897e4afbde50acdd7237f46c
Sherveen Mehrvarzan <shervm@uw.edu>
2023-09-17 20:02:21 -0700

    Test Code
    Dummy code to help get used to the libraries

---
+++ mainTest.py
@@ -0,0 +1,17 @@
+import cv2
+import os
+import time
+
+cap = cv2.VideoCapture(0)
+pTime = 0
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cTime = time.time()
+    print(cTime)
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img,f'FPS:{int(fps)}',(20,70),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.imshow("Test", img)
+
+    cv2.waitKey(1)
